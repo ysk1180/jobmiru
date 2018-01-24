d4e964a51e6ec43c86ee6b4178cd15296729b921
自分の名前を入力してください <yusuke@YusukenoMacBook-Air.local>
2018-01-24 17:05:42 +0900

    1/24_17:05

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact)
    @contact = contact
 
-   mail to: "ysk110@gmail.com", subject: "Jobmiruへのお問い合わせ"
+   mail to: "ysk1180@gmail.com", subject: "Jobmiruへのお問い合わせ"
  end
 end
