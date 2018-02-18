a46de780967d4c30e488b23273b759ff1a37eb8a
Yusuke Miyazaki <ysk1180@gmail.com>
2018-02-18 17:17:51 +0900

    Closes #7  何度かに分けてメソッド共通化完了

--- app/controllers/post_comment_replies_controller.rb
+++ app/controllers/post_comment_replies_controller.rb
@@ -1,16 +1,16 @@
 class PostCommentRepliesController < ApplicationController
+  before_action :set_post, only: [:create, :destroy]
+  before_action :set_comment, only: [:create, :destroy]
 
   def new
     @post_comment_reply = PostCommentReply.new
   end
 
   def create
-    @post = Post.find(params[:post_id])
     @post_comment_reply = @post.post_comment_replies.build(post_comment_reply_params)
     @post_comment_reply.user_id = current_user.id
     @post_comment_reply.post_comment_id = params[:post_comment_id]
-    @post_comment = PostComment.find(params[:post_comment_id])
-    @post_comment_replies = @post.post_comment_replies.includes(:user).all
+    set_replies
     if @post_comment_reply.save
       render :index
     end
@@ -19,14 +19,24 @@ class PostCommentRepliesController < ApplicationController
   def destroy
     @post_comment_reply = PostCommentReply.find(params[:id])
     @post_comment_reply.destroy
-    @post_comment = PostComment.find(params[:post_comment_id])
-    @post = Post.find(params[:post_id])
-    @post_comment_replies = @post.post_comment_replies.includes(:user).all
+    set_replies
     render :index
   end
 
   private
     def post_comment_reply_params
       params.require(:post_comment_reply).permit(:content, :post_id, :user_id, :post_comment_id)
+    end
+
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def set_comment
+      @post_comment = PostComment.find(params[:post_comment_id])
+    end
+
+    def set_replies
+      @post_comment_replies = @post.post_comment_replies.includes(:user).all
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-
 Rails.application.routes.draw do
   resources :contacts, only: [:new, :create]
 
