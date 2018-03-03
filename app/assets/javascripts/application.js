5d019688d9cbd11b24db1b4131661d6acc9d4ae3
Yusuke Miyazaki <ysk1180@gmail.com>
2018-03-03 15:21:26 +0900

    レイアウト修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
-//= require Chart.bundle
-//= require chartkick
 //= require rails-ujs
 //= require turbolinks
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
-
+//= require Chart.bundle
+//= require chartkick
 //= require_tree .
