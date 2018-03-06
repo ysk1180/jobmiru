58ce318fccdd76c117dc0f6673321418949cd730
Yusuke Miyazaki <ysk1180@gmail.com>
2018-03-06 20:14:32 +0900

    Closes #24

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,6 +3,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"].except("extra"))
     if @user.persisted?
       sign_in_and_redirect @user
+      set_flash_message(:notice, :success, kind: "twitter") if is_navigational_format?
     else
       session["devise.user_attributes"] = @user.attributes
       redirect_to new_user_registration_url
