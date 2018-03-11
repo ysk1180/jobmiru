59a364a15fba0088df5f70aab6b8171aae17bae2
Yusuke Miyazaki <ysk1180@gmail.com>
2018-03-11 10:15:22 +0900

    入力フォーム バリデーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,17 @@ class Post < ApplicationRecord
   validates :job_end1, presence: true
   validates :job_end1, numericality: { greater_than: :job_start }
 
+  validates :skill1, presence: true
+  validates :skill2, presence: true
+  validates :skill3, presence: true
+  validates :skill4, presence: true
+  validates :skill5, presence: true
+  validates :skill_level1, presence: true
+  validates :skill_level2, presence: true
+  validates :skill_level3, presence: true
+  validates :skill_level4, presence: true
+  validates :skill_level5, presence: true
+
   has_many :likes, dependent: :destroy
   has_many :like_users, through: :likes, source: :user
 
