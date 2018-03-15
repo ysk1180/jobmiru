9c8a14c679ac4485df341581f132f980389e30e5
Yusuke Miyazaki <ysk1180@gmail.com>
2018-03-15 20:06:57 +0900

    投稿のバリデーションの重複、順番調整

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,28 +1,27 @@
 class Post < ApplicationRecord
-  validates :job_content1, presence: true
-  validates :obtained_skill, presence: true
   validates :company_name, presence: true
   validates :industry, presence: true
   validates :job_category, presence: true
   validates :job_start, presence: true
-  validates :job_end1, presence: true
-  validates :job_end1, numericality: { greater_than: :job_start }
-
+  validates :job_content1, presence: true
   validates :job_content1, length: { maximum: 15 }
+  validates :job_end1, presence: true
+  validates :job_end1, numericality: { greater_than: :job_start }, allow_blank: true
+
   validates :job_content2, length: { maximum: 15 }
   validates :job_content3, length: { maximum: 15 }
   validates :job_content4, length: { maximum: 15 }
   validates :job_content5, length: { maximum: 15 }
 
   validates :skill1, presence: true
-  validates :skill2, presence: true
-  validates :skill3, presence: true
-  validates :skill4, presence: true
-  validates :skill5, presence: true
   validates :skill_level1, presence: true
+  validates :skill2, presence: true
   validates :skill_level2, presence: true
+  validates :skill3, presence: true
   validates :skill_level3, presence: true
+  validates :skill4, presence: true
   validates :skill_level4, presence: true
+  validates :skill5, presence: true
   validates :skill_level5, presence: true
 
   validates :skill1, length: { maximum: 10 }
@@ -30,6 +29,8 @@ class Post < ApplicationRecord
   validates :skill3, length: { maximum: 10 }
   validates :skill4, length: { maximum: 10 }
   validates :skill5, length: { maximum: 10 }
+
+  validates :obtained_skill, presence: true
 
   has_many :likes, dependent: :destroy
   has_many :like_users, through: :likes, source: :user
