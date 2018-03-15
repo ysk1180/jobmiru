6d0392dc19abf6ee25a0c0ef1160d4edef82b58e
Yusuke Miyazaki <ysk1180@gmail.com>
2018-03-15 21:24:36 +0900

    close #42 お問い合わせのバリデーション改善

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, allow_blank: true
   validates :content, presence: true
 end
