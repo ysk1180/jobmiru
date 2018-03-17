556ce5abbd0fae0ccb561c22b96a8c1b6b6ba700
Yusuke Miyazaki <ysk1180@gmail.com>
2018-03-17 19:01:21 +0900

    close #74 仕事内容のバリデーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,9 +9,51 @@ class Post < ApplicationRecord
   validates :job_end1, numericality: { greater_than: :job_start }, allow_blank: true
 
   validates :job_content2, length: { maximum: 15 }
+  validates :job_content2, presence: true, if: :job_end2_present?
+  validates :job_end2, numericality: { greater_than: :job_end1 }, if: :job_content2_present?
   validates :job_content3, length: { maximum: 15 }
+  validates :job_content3, presence: true, if: :job_end3_present?
+  validates :job_end3, numericality: { greater_than: :job_end2 }, if: :job_content3_present?
   validates :job_content4, length: { maximum: 15 }
+  validates :job_content4, presence: true, if: :job_end4_present?
+  validates :job_end4, numericality: { greater_than: :job_end3 }, if: :job_content4_present?
   validates :job_content5, length: { maximum: 15 }
+  validates :job_content5, presence: true, if: :job_end5_present?
+  validates :job_end5, numericality: { greater_than: :job_end4 }, if: :job_content5_present?
+  validates :job_content6, length: { maximum: 15 }
+  validates :job_content6, presence: true, if: :job_end6_present?
+  validates :job_end6, numericality: { greater_than: :job_end5 }, if: :job_content6_present?
+
+  def job_end2_present?
+    job_end2.present?
+  end
+  def job_content2_present?
+    job_content2.present?
+  end
+  def job_end3_present?
+    job_end3.present?
+  end
+  def job_content3_present?
+    job_content3.present?
+  end
+  def job_end4_present?
+    job_end4.present?
+  end
+  def job_content4_present?
+    job_content4.present?
+  end
+  def job_end5_present?
+    job_end5.present?
+  end
+  def job_content5_present?
+    job_content5.present?
+  end
+  def job_end6_present?
+    job_end6.present?
+  end
+  def job_content6_present?
+    job_content6.present?
+  end
 
   validates :skill1, presence: true
   validates :skill1, length: { maximum: 12 }
