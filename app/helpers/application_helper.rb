fa3bcb88020461d882eeb4e7743c61aa68ae44a1
Yusuke Miyazaki <ysk1180@gmail.com>
2018-03-18 16:10:04 +0900

    Twitterカード試行10

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
       twitter_card[:url] = 'https://jobmiru.herokuapp.com/'
       twitter_card[:title] = 'Jobmiru〜「何が出来るようになるか」で会社を選べる世界に'
       twitter_card[:description] = '「何のスキルが身につくのか」「具体的に何をするのか」に特化した、実際に働いた社員の声の共有サイトです。'
-      twitter_card[:image] = "https://raw.githubusercontent.com/ysk1180/jobmiru/master/app/assets/images/top_shot4.png?3253"
+      twitter_card[:image] = "https://raw.githubusercontent.com/ysk1180/jobmiru/master/app/assets/images/top_shot4.png?32253"
     end
     twitter_card[:card] = 'summary_large_image'
     twitter_card[:site] = '@ysk_pro'
