6f0bde7c6f7b599d1ad1a19ade4d03b378d3a255
Yusuke Miyazaki <ysk1180@gmail.com>
2018-03-18 16:26:56 +0900

    Twitterカード試行11

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,18 +2,17 @@ module ApplicationHelper
   def get_twitter_card_info(post)
     twitter_card = {}
     if post
-      twitter_card[:url] = post.url
-      twitter_card[:title] = '会社名：#{post.name}'
-      twitter_card[:description] = "スキル："
-      twitter_card[:image] = set_post_img(post.image_src)
+      twitter_card[:url] = 'https://jobmiru.herokuapp.com/post/#{post.id}'
+      twitter_card[:title] = 'Jobmiruへの投稿です。'
+      twitter_card[:description] = '会社名：#{post.name}　身についたスキル：#{post.skill1}、#{post.skill2}、#{post.skill3}'
     else
       twitter_card[:url] = 'https://jobmiru.herokuapp.com/'
       twitter_card[:title] = 'Jobmiru〜「何が出来るようになるか」で会社を選べる世界に'
       twitter_card[:description] = '「何のスキルが身につくのか」「具体的に何をするのか」に特化した、実際に働いた社員の声の共有サイトです。'
-      twitter_card[:image] = "https://raw.githubusercontent.com/ysk1180/jobmiru/master/app/assets/images/top_shot4.png?32253"
     end
     twitter_card[:card] = 'summary_large_image'
-    twitter_card[:site] = '@ysk_pro'
+    twitter_card[:image] = "https://raw.githubusercontent.com/ysk1180/jobmiru/master/app/assets/images/top_shot4.png?32253"
+    # twitter_card[:site] = '@ysk_pro'
     twitter_card
   end
 end
