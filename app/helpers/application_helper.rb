338022d823f6aa95674176dd88a931f0bd91ff38
Yusuke Miyazaki <ysk1180@gmail.com>
2018-03-19 19:50:45 +0900

    Twitterカード文言変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     if post
       twitter_card[:url] = "https://jobmiru.herokuapp.com/posts/#{post.id}"
       twitter_card[:title] = 'Jobmiruへの投稿'
-      twitter_card[:description] = "会社名：#{post.company_name}　身についたスキル：#{post.skill1}、#{post.skill2}、#{post.skill3}"
+      twitter_card[:description] = "会社名：#{post.company_name}で身についたスキルは、#{post.skill1}、#{post.skill2}、#{post.skill3}です！"
     else
       twitter_card[:url] = 'https://jobmiru.herokuapp.com/'
       twitter_card[:title] = 'Jobmiru〜「何が出来るようになるか」で会社を選べる世界に'
