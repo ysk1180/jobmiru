25221ed9d2e0a059b595581e64e5566d34eb35b1
Yusuke Miyazaki <ysk1180@gmail.com>
2018-04-07 08:17:25 +0900

    スムーススクロールエラー対応

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require turbolinks
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
 //= require Chart.bundle
