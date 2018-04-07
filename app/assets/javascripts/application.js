418830c79e0bc927e038d58dbe466b589429dbba
Yusuke Miyazaki <ysk1180@gmail.com>
2018-04-07 21:04:51 +0900

    ターボリンクス削除

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
 //= require Chart.bundle
